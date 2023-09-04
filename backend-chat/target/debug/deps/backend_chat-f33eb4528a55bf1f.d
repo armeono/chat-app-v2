e26bb18f301ccdf3de86a2fdc300ceef799b2664
armeono <arman.hadzigrahic@zendev.se>
2023-09-04 15:42:31 +0200

    initialized backend

---
+++ backend-chat/target/debug/deps/backend_chat-f33eb4528a55bf1f.d
@@ -0,0 +1,5 @@
+/Users/armanhadzigrahic/programming/chat-app-v2/backend-chat/target/debug/deps/backend_chat-f33eb4528a55bf1f.rmeta: src/main.rs
+
+/Users/armanhadzigrahic/programming/chat-app-v2/backend-chat/target/debug/deps/backend_chat-f33eb4528a55bf1f.d: src/main.rs
+
+src/main.rs:
